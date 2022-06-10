d19a1ddd57584bee788c449bf9ae2c131e7f062d
JBarrada <lephtbrain@gmail.com>
2022-06-10 00:05:31 -0500

    discrete colors

--- default_clock/hexclock.py
+++ default_clock/hexclock.py
@@ -9,8 +9,8 @@ display.driver.init()
 def show_clock():
     hour = datetime.datetime.now().hour
     hour_text = format(hour, 'X') if hour < 13 else format(hour-12, 'X')
-    hour_color = color_tools.h_to_rgb((datetime.datetime.now().minute/15)*90)
-    display.show_text(hour_text, 3, 1, 0, hour_color)
+    quater_colors = [(255, 42, 0), (0, 255, 42), (42, 0, 255), (140, 0, 255)]
+    display.show_text(hour_text, 3, 1, 0, quater_colors[int(datetime.datetime.now().minute/15)])
 
 
 show_clock()
